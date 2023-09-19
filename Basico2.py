05364b0430410afc4e4dfda89f64059e0defd125
VictorSainos <vsainosu2200@alumno.ipn.mx>
2023-09-19 15:53:27 -0600

    Basico2,py

---
+++ Basico2.py
@@ -0,0 +1,60 @@
+#=========================
+# Jesus Mauricio Carrillo
+#==============================
+#   Paradigmas de programación.
+#   Matematica Algoritmica 
+#   ESFM-IPN Sept 2023
+#====================================================
+#Input permite obtener datos del usuario en prompter
+#=====================================================
+nombre = input ("Dame tu nombre")
+print ("Hola como estas", nombre)
+#=====================================================
+#   Los enteros son de precision ilimitada
+#====================================================
+y= 5000000000000000000000000000000000000000000000000
+print(y)
+
+#===========================================================
+# Se puede delimitar numeros con guion bajo pero no con coma
+#===========================================================
+y = 5_000_000
+print(y)
+#=============================================================
+# La funcion int() cambia strings a floats y enteros
+#=============================================================
+numero= int(input("Dame tu edad: "))
+type(numero)
+#=============================================================
+# La notacion cientifica de flotantres utiliza e o E
+#=============================================================
+# 1.2 x 10⁻⁹
+#=============================================================
+y= 1.2E-09
+print(y)
+#=============================================================
+# La funcion float() convierte strings y enteros a floats
+#=============================================================
+y=float("14.13")
+print(y)
+
+#============================================================
+# Los complejos se escriben con la raiz de menos uno 
+# j siempre como un numero como prefijo
+# No aceptas la j suelta
+#==============================================================
+z = 1+1j
+
+# suma + 
+# resta -
+# multiplicacion * 
+#division /
+# modulo %
+# exponente **
+# // funcion piso
+# funciones para transformar numeros int() float() complex()
+# Operaciones abs() round() pow()
+
+print (round(3.14159,4)
+
+
