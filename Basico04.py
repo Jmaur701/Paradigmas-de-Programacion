561fe95923470754f1d76b72d9b59f5997224834
VictorSainos <vsainosu2200@alumno.ipn.mx>
2023-09-26 15:53:48 -0600

    Basico04.py

---
+++ Basico04.py
@@ -0,0 +1,22 @@
+#=========================
+# Jesus Mauricio Carrillo
+#==============================
+#   Paradigmas de programación.
+#   Matematica Algoritmica 
+#   ESFM-IPN Sept 2023
+#====================================================
+# Conjunto en Python
+#==================================================
+even_nums = {2, 4, 6, 8, 10}
+print(even_nums)
+
+# El bool no es una parte del conjunto
+emp = {1,'Steve', 10.5, True) # Conjunto de diferentes objetos
+print(emp)
+
+nums = {1, 2, 2, 3, 4, 4, 5, 5}
+print(nums)
+
+#==============================================
+# Converttir la secuencia a conjunto 
+# No lo genera 
